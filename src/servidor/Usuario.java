8956b0bf032a3a6125ef27e39ee259fcc33c2842
Fatake Raicos <pace_lozano@hotmail.com>
2020-05-12 11:16:20 -0500

    Paquete Servidor creado

---
+++ src/servidor/Usuario.java
@@ -0,0 +1,54 @@
+package servidor;
+
+import java.util.ArrayList;
+
+public class Usuario {
+    private String nombre;
+    private String password;
+    private ArrayList<Usuario> amigos = new ArrayList<>();
+
+    
+    //
+    // Constructores
+    //
+    public Usuario (){
+        this.nombre = "";
+        this.password = "";
+    }
+    public Usuario (String nombre, String password){
+        this.nombre = nombre;
+        this.password = password;
+    }
+    public Usuario (String nombre, String password,ArrayList<Usuario> amigos){
+        this.nombre = nombre;
+        this.password = password;
+        this.amigos = amigos;
+    }
+    
+    //
+    // Getters and setters
+    //
+    public String getName(){
+        return this.nombre;
+    }
+    public String getPass(){
+        return this.password;
+    }
+    public ArrayList<Usuario> getAmigos(){
+        return this.amigos;
+    }
+    public void setName(String name){
+        this.nombre = name;
+    }
+    public void setPass(String password){
+        this.password = password;
+    }
+    public vois setAmigos(ArrayList<Usuario> amigos){
+        this.amigos = amigos;
+    }
+    
+    public String toString(){
+        String aux = "Nombre: "+this.nombre+" Contraseña: "+this.password;
+        return aux;
+    }
+}
